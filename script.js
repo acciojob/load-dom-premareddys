6968ffd9b45e522c18e9f5bb53aa538f332097d9
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-22 18:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your JS code here. If required.
+/*function body(){
 document.body.innertext="DOM load success";
+}*/
+function body() {
+        document.body.textContent = "DOM load success";
+      }
+      document.addEventListener("DOMContentLoaded", body);
